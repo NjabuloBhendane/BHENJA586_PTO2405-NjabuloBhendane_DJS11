e3c11670a1e27d42f2d3a60e71d1e8cf0a5cfe2b
Njabulo Bhendane <njabulobhendane1996@gmail.com>
2025-03-30 18:24:38 +0200

    testing netlify changes

--- podcastapp/src/App.jsx
+++ podcastapp/src/App.jsx
@@ -2,28 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
   return (
-  <>  <div>
-        <p>Hello Njabulo</p>
-      </div>
-    <div>
-      <p> You doing great</p>
-    </div>
-    <Page/>
-
+  <>  
+ < MovieCard movie = {{title:"Njabulo's film", release_date: "2024"}}/> 
+ < MovieCard movie = {{title:"Mpumis's film", release_date: "2025 "}}/>
   </>
     
   )
 }
 
-function Page() {
-  return (
-    <div>
-    <p>I really want to learn how to code to better my life and the lifes of the people around me</p>
-  </div>
-  )
-}
+
 
 export default App
