0fe59219b1012c0e74039294928707f7fb055ebb
Njabulo Bhendane <njabulobhendane1996@gmail.com>
2025-05-31 18:20:10 +0200

    deploy test

--- podcastapp/src/components/Navbar.jsx
+++ podcastapp/src/components/Navbar.jsx
@@ -1,65 +1,19 @@
 import { Link } from "react-router-dom";
-import { searchPodcasts } from "../services/podcastApi";
-import {useState} from "react";
+
 
 function Navbar() {
-  const [searchQuery, setSearchQuery] = useState("");
-   const [podcasts, setPodcasts] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-  
+
   function handleFavoritesClick() {
     alert("clicked");
   }
 
-  const handleSearch = async (e) => {
-  e.preventDefault();
-
-  const trimmedQuery = searchQuery.trim();
-  if (!trimmedQuery || loading) return;
-
-  setLoading(true);
-  setError(null); // clear any previous errors
-
-  try {
-    const searchResults = await searchPodcasts(trimmedQuery);
-
-    if (!searchResults || searchResults.length === 0) {
-      setError("No podcasts found.");
-      setPodcasts([]); // clear previous results
-    } else {
-      setPodcasts(searchResults);
-    }
-
-  } catch (err) {
-    console.error("Search error:", err);
-    setError("Failed to search podcasts. Please try again.");
-  } finally {
-    setLoading(false);
-  }
-};
-
-
-
-  return (
+ return (
     <header className="nav-bar">
       <Link className="site-logo" to="/">
         PodcastApp
       </Link>
       <nav>
         <Link to="/">Home</Link>
-         <form onSubmit={handleSearch} className="search-form">
-        <input
-          type="text"
-          placeholder="Search for movies..."
-          className="search-input"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button type="submit" className="search-button">
-          Search
-        </button>
-      </form>
         <button className="favorite-btn" onClick={handleFavoritesClick}>
           <span>Favourite ♡</span>
         </button>
--- podcastapp/src/services/podcastApi.ts
+++ podcastapp/src/services/podcastApi.ts
@@ -1,36 +1,17 @@
-
-import { PodcastPreview , Show } from "../types/podcast";
+import { PodcastPreview, Show } from "../types/podcast";
 
 const API_URL = "https://podcast-api.netlify.app";
 const API_KEY = "";
 
-export const fetchALLShows = async (): Promise <PodcastPreview[]>  => {
-try {
-    const response = await fetch (`${API_URL}`);
- if (!response.ok) {
-    throw new Error(`API error:${response.status}`);
- }
-return await response.json();
-} catch(error) {
+export const fetchALLShows = async (): Promise<PodcastPreview[]> => {
+  try {
+    const response = await fetch(`${API_URL}`);
+    if (!response.ok) {
+      throw new Error(`API error:${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
     console.error("Error Fetching shows", error);
     throw error;
-}
-}
-
-export const searchPodcasts = async (query) => {
-  try {
-    const response = await fetch(
-      `${API_URL}/search/podcast?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
-    );
-
-    if (!response.ok) {
-      throw new Error(`API error: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    console.error("Failed to search podcasts:", error);
-    return []; // or throw the error if you want to handle it higher up
   }
 };
